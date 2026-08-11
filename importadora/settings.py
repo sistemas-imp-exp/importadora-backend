4106a4ae18032a9038d9668e849824fd1071f73c
sistemas-imp-exp <sistemas@importadorademariscos.com>
2026-08-11 16:29:09 -0600

    Agregando regla de ruta para archivos staticos en producción

--- importadora/settings.py
+++ importadora/settings.py
@@ -146,12 +146,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_URL = 'static/'
-
-# Archivos subidos por usuarios (adjuntos de movimientos, etc.)
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# Media files (archivos subidos por usuarios)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
