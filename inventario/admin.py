d437e2e582402e816eb0bdb301d5bed8036389c4
sistemas-imp-exp <sistemas@importadorademariscos.com>
2026-08-22 12:44:45 -0600

    Registrar entidades de inventario en el admin de Django

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,2 +1,27 @@
 from django.contrib import admin
-from inventario.models import *
+
+from inventario.models import (
+    Camara,
+    Cliente,
+    Empresa,
+    Entrada,
+    EntradaDetalle,
+    LoteGeneral,
+    MovimientoCamara,
+    Producto,
+    Proveedor,
+    Salida,
+    SalidaDetalle,
+)
+
+admin.site.register(Empresa)
+admin.site.register(Camara)
+admin.site.register(Proveedor)
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Entrada)
+admin.site.register(LoteGeneral)
+admin.site.register(EntradaDetalle)
+admin.site.register(Salida)
+admin.site.register(SalidaDetalle)
+admin.site.register(MovimientoCamara)
